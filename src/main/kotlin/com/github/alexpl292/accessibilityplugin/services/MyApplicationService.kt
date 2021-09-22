e99bce4fb05a3570e04f181deb2d831df767b8b8
GitHub Action <action@github.com>
2021-09-22 07:44:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alexpl292/accessibilityplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alexpl292.accessibilityplugin.services
+
+import com.github.alexpl292.accessibilityplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
